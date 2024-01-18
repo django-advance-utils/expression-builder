5790a32f9871a0b99430d0acb5c072c4408356c7
Tom Turner <tom@twt.me.uk>
2024-01-18 09:18:33 +0000

    Updated the version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="expression-builder",
-    version="0.0.4",
+    version="0.0.5",
     author="Tom Turner",
     description="Django app to add created and modified",
     long_description=long_description,
