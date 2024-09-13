c4ea4032e22eb2a442f23bdb55fb3df30caaaaa4
Tom Turner <tom@twt.me.uk>
2024-09-13 16:49:02 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="expression-builder",
-    version="0.0.11",
+    version="0.0.12",
     author="Tom Turner",
     description="Django app that does mathematical expression",
     long_description=long_description,
